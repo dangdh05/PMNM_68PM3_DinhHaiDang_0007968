b66aa0968cbdec323b30d5eb711b6f7899eec4b2
dangdh05 <dangdh05@gmail.com>
2026-05-26 22:06:05 +0700

    feat: Hiển thị danh sách sinh viên

--- App/controllers/auth.php
+++ App/controllers/auth.php
@@ -1,18 +1,19 @@
 <?php
 class auth
 {
-    protected $user = array(
-        'admin' => '123456',
-        'dang' => '123456'
-    );
+    protected $user = [
+        'admin' => '2',
+        'ngoc' => '2'
+    ];
     public function login()
     {
-        if (isset($_POST['username']) && isset($_POST['password'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
+
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'] ?? '';
+            $password = $_POST['password'] ?? '';
             if (isset($this->user[$username]) && $this->user[$username] === $password) {
                 $_SESSION['username'] = $username;
-                header('Location: /home/index');
+                header('Location: /sinhvien/index');
                 exit();
             } else {
                 header('Location: /home/login');
--- App/controllers/sinhvien.php
+++ App/controllers/sinhvien.php
@@ -1,13 +1,20 @@
+
 <?php
-class sinhvien
+require_once '../app/core/Controller.php';
+class sinhvien extends Controller
 {
-    public function index()
+    function index()
     {
-        //trả về view
-        require_once '../app/views/sinhvien/index.php';
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvien = $sinhvienModel->getAllSinhvien();
+        //trả về view 
+        //require_once '../app/views/sinhvien/index.php';
+        $this->view('sinhvien/index', ['sinhvien' => $sinhvien]);
     }
-    public function create()
+
+    function create()
     {
+        //trả về view 
         require_once '../app/views/sinhvien/create.php';
     }
 }
